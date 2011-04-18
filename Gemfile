cda3ec5374d22173725aa5797b93d030a6de2540
falconlux <luxiang@dislab.nju.edu.cn>
2011-04-18 14:29:45 +0800

    first commit

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'http://rubygems.org'
+ 
+gem 'rails', '3.0.5'
+gem 'gravatar_image_tag'
+gem 'will_paginate', '3.0.pre2'
+gem 'jquery-rails'
+gem 'faker'
+# Bundle edge Rails instead:
+# gem 'rails', :git => 'git://github.com/rails/rails.git'
+
+gem 'mysql'
+group :development do
+    gem 'rspec-rails'
+    gem 'annotate-models'
+end
+          
+ group :test do 
+      gem 'rspec'
+      gem 'webrat'
+      gem 'spork'
+      gem 'factory_girl_rails'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zhaochide::Application.config.secret_token = 'f77dca7ace0b66eea75892bd8dea920cb4d52100baf69cc4ad6e293b95a2a06e68c95b48f0ce4acdfd8e660c8f064917785ab1bc661b70a90d630633d8b7b87f'
